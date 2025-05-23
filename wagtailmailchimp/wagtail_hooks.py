63b40ebdd53e5f742eb571e2c11418e4f4edde9d
Erick Otenyo <otenyo.erick@gmail.com>
2025-05-23 11:37:39 +0300

    Use ListingButton instead of PageListingButton

--- wagtailmailchimp/wagtail_hooks.py
+++ wagtailmailchimp/wagtail_hooks.py
@@ -17,7 +17,7 @@ def page_listing_buttons(page, user, next_url=None):
     if hasattr(page, "is_mailchimp_integration") and hasattr(page, "audience_list_id"):
         if page.audience_list_id and page.show_page_listing_mailchimp_integration_button():
             url = reverse("mailchimp_integration_view", args=[page.pk, ])
-            yield wagtail_admin_widgets.PageListingButton(
+            yield wagtail_admin_widgets.ListingButton(
                 "Mailchimp Integration",
                 url,
                 priority=60
